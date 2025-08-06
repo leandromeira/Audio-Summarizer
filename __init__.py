6a593ea742a6fbe1a769d871efa31b60e3bea3a3
Leandro Meira Marinho Queiroz <leandro.meira@unesp.br>
2025-08-06 18:37:11 -0300

    Bump version to 2.0.0 in __init__.py

--- __init__.py
+++ __init__.py
@@ -9,10 +9,10 @@ Run with: ./run.sh or python3 main.py
 
 Author: Leandro Meira Marinho Queiróz
 License: MIT
-Version: 1.0.0
+Version: 2.0.0
 """
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = "Leandro Meira Marinho Queiróz"
 __license__ = "MIT"
 __description__ = "Intelligent audio transcription and summarization tool (standalone)"
